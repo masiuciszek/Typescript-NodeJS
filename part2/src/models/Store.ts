b5c6cac3ec5064eea0110d7f48ac5331bfcac633
Masiu <ciszekmarcell@gmail.com>
2020-01-02 12:50:18 +0100

    store schema

--- part2/src/models/Store.ts
+++ part2/src/models/Store.ts
@@ -1,1 +1,41 @@
-import mongoose from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IStore extends Document {
+  storeId: string;
+  address: string;
+  location: string;
+  coordinates: number[];
+  formattedAddress: string;
+  createdAt: Date;
+}
+
+const storeSchema: Schema = new Schema({
+  storeId: {
+    type: String,
+    required: [true, 'Please add a store ID'],
+    unique: true,
+    trim: true,
+    maxlength: [10, 'Store ID must be less than 10 chars'],
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address'],
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere',
+    },
+    formattedAddress: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model<IStore>('Store', storeSchema);
