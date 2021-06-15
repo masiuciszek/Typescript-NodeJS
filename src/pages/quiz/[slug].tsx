33bdacf29ca11c8e217bd9976346601cb4cafc52
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-06-15 17:50:48 +0200

    feat: hooks

--- src/pages/quiz/[slug].tsx
+++ src/pages/quiz/[slug].tsx
@@ -2,12 +2,34 @@ import {useRouter} from "next/dist/client/router"
 
 interface Props {}
 
+const END_POINTS = {
+  WITH_STATE: "with-state",
+  WITH_REDUCER: "with-reducer",
+  WITH_XSTATE: "with-xstate",
+  WITH_RECOIL: "with-recoil",
+} as const
+
+// TODO: Func to decide what quiz app we will render
+const getQuizApp = (endPoint: keyof typeof END_POINTS | undefined) => {
+  if (endPoint) {
+    switch (endPoint) {
+      case "WITH_STATE":
+        return "with-state-component"
+
+      default:
+        return "message component"
+    }
+  }
+  return "message"
+}
+
 const QuizItem = () => {
   const router = useRouter()
-  console.log(router.query)
+  const slug = router.query.slug as keyof typeof END_POINTS
   return (
     <div>
       <h1>QuizItem</h1>
+      {getQuizApp(slug)}
     </div>
   )
 }
